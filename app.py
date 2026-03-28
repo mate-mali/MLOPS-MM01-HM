f5dff1070e4b920d4e0129d5046cfa116407352c
mate-mali <mateusz.malinowski.1991.gmail.com>
2026-03-28 14:53:28 +0100

    going back to add commit hooks and moving inference part from app.py to infference.py

--- app.py
+++ app.py
@@ -1,34 +1,26 @@
 from fastapi import FastAPI
-import joblib
 import uvicorn
-import sentence_transformers as st
+from inference import load_classifier, load_sentence_transformer, map_prediction_to_sentiment
 from api.models.sentiment import ze_input, ze_output
 
-
+#initiate fastapi app
 app = FastAPI()
 
-def load_classifier():
-    model = joblib.load("model/classifier.joblib")
-    return model
-
-def load_sentence_transformer():
-    model = st.SentenceTransformer('model/sentence_transformer.model')
-    return model
-
-def map_prediction_to_sentiment(prediction):
-    preds = {0: "negative", 1: "neutral", 2: "positive"}
-    return preds.get(prediction, "unknown")
-
-
+#generic endpoint to welcome people
 @app.get("/")
 def welcome_root():
-    return {"message": "Mateusz Malinowski fancy szmancy API"}
+    return {"message": "For better curling experience, please use /docs endpoint "}
 
+
+#actual predicting endpoint, it was tested in english and is quite hard to get neutral sentimen out of it
 @app.post("/predict")
 def predict_sentiment(input: ze_input): 
+    '''Endpoint to predict sentiment of the input text\
+    Args:        input (ze_input): Input text for sentiment analysis that is provided on post request'''
     classifier = load_classifier()
     sentence_transformer = load_sentence_transformer()
     text_embedding = sentence_transformer.encode(input.text)
     prediction = classifier.predict([text_embedding])
+    #added som minor intermiary dictionary fro lookup fro values 0-2
     ze_sentiment = map_prediction_to_sentiment(prediction[0])
     return ze_output(prediction=ze_sentiment)
---
+++ inference.py
@@ -0,0 +1,20 @@
+import joblib
+import sentence_transformers as st
+
+
+#wrapper fro laoding model and returning it using joblin
+def load_classifier():
+    #load classifier using joblib
+    model = joblib.load("model/classifier.joblib")
+    return model
+
+#wrapper fro loading mdoel transformer from folder
+def load_sentence_transformer():
+    #load fodler with this transformer thingy
+    model = st.SentenceTransformer('model/sentence_transformer.model')
+    return model
+
+#thrwo results of classifier into dictionary and return text
+def map_prediction_to_sentiment(prediction):
+    preds = {0: "negative", 1: "neutral", 2: "positive"}
+    return preds.get(prediction, "unknown")
